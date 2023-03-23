90cb489166df0454c1fd6b6b583ab5bdf5093eb5
Kristofer Krause <kris.krause@gmail.com>
2023-03-23 09:28:30 -0400

    separate interface, files are cheap

--- TastyBot.Library/TastyBot.cs
+++ TastyBot.Library/TastyBot.cs
@@ -6,22 +6,6 @@ using TastyBot.Rules;
 
 namespace TastyBot.Library
 {
-    public interface ITastyBot
-    {
-        Task<TastySessionInfo> getAuthorization();
-        Task<TastyAccountInfo> getAccounts();
-        Task<TastyBalance> getBalance(string accountId);
-        Task<TastyMetrics> getMarketMetrics(string tickers);
-        Task<TastyChain> getOptionChain(string ticker);
-        Task<TastyStreamer> getStreamerTokens();
-        Task<TastyOrderData> doDryRun(string accountId, TastySpread spread);
-        Task<TastyOrderData> placeOrder(string accountId, TastySpread spread);
-        Task<TastyPositionData> getPositions(string accountId);
-        Task<TastyLiveOrderData> getOrders(string accountId);
-        Task<List<RuleResult>> processRules();
-        Task Terminate();
-    }
-
     public class TastyBot : ITastyBot
     {
         private readonly string _secretName;
